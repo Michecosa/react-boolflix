c34ddc19c4ace1372b440935bab1835fd4231f51
Michela <micheladellagatta1@gmail.com>
2025-11-19 17:42:48 +0100

    Fix card image size and values

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,28 @@ function Card({ movie }) {
   return (
     <div className="col-md-3 mb-4" key={movie.id}>
       <div className="card h-100">
-        {movie.poster_path ? (
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.name || movie.title}
-          />
-        ) : (
-          <div
-            className="card-img-top d-flex align-items-center justify-content-center bg-secondary text-white"
-            style={{ height: "300px" }}
-          >
-            No image found
-          </div>
-        )}
+        <div style={{ height: "390px", overflow: "hidden" }}>
+          {movie.poster_path ? (
+            <img
+              src={`https://image.tmdb.org/t/p/w500${
+                movie.poster_path || movie.backdrop_path
+              }`}
+              alt={movie.name || movie.title}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          ) : (
+            <div
+              className="d-flex align-items-center justify-content-center bg-secondary text-white"
+              style={{ height: "100%" }}
+            >
+              No image found
+            </div>
+          )}
+        </div>
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">{movie.name || movie.title}</h5>
           <p className="card-text" style={{ flexGrow: 1 }}>

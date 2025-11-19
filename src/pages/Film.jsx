f15e7b0d2ddc3e3a79be5e099290161498f73ade
Michela <micheladellagatta1@gmail.com>
2025-11-19 17:46:10 +0100

    Move Homepage structure in "Film" page

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,5 +1,28 @@
+import { useContext, useEffect } from "react";
+import { SearchContext } from "../context/SearchContext";
+import Card from "../components/Card";
+
 function Film() {
-  return <div>Film</div>;
+  const { movies, setMovies, setQuery } = useContext(SearchContext);
+
+  useEffect(() => {
+    setMovies([]);
+    setQuery("");
+  }, [setMovies, setQuery]);
+
+  return (
+    <div className="container mt-4">
+      {movies.length > 0 ? (
+        <div className="row">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center mt-4">Nothing here</p>
+      )}
+    </div>
+  );
 }
 
 export default Film;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,5 @@
-import { useContext, useEffect } from "react";
-import { SearchContext } from "../context/SearchContext";
-import Card from "../components/Card";
-
 function Home() {
-  const { movies, setMovies, setQuery } = useContext(SearchContext);
-
-  useEffect(() => {
-    setMovies([]);
-    setQuery("");
-  }, [setMovies, setQuery]);
-
-  return (
-    <div className="container mt-4">
-      {movies.length > 0 ? (
-        <div className="row">
-          {movies.map((movie) => (
-            <Card key={movie.id} movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center mt-4">Nothing here</p>
-      )}
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;

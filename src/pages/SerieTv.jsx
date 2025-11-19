8ed976c49cdabc6ac3502a60e2e523c0da40c70c
Michela <micheladellagatta1@gmail.com>
2025-11-19 17:25:14 +0100

    Transform series infos in cards

--- src/pages/SerieTv.jsx
+++ src/pages/SerieTv.jsx
@@ -10,17 +10,46 @@ function SerieTv() {
   }, [setMovies, setQuery]);
 
   return (
-    <div>
+    <div className="container mt-4">
       {movies.length > 0 ? (
-        <ul>
+        <div className="row">
           {movies.map((movie) => (
-            <li key={movie.id}>
-              {movie.original_name} ({movie.overview})
-            </li>
+            <div className="col-md-3 mb-4" key={movie.id}>
+              <div className="card h-100">
+                {movie.poster_path ? (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.name}
+                  />
+                ) : (
+                  <div
+                    className="card-img-top d-flex align-items-center justify-content-center bg-secondary text-white"
+                    style={{ height: "300px" }}
+                  >
+                    No image found
+                  </div>
+                )}
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">{movie.name}</h5>
+                  <p className="card-text" style={{ flexGrow: 1 }}>
+                    {movie.overview
+                      ? movie.overview
+                      : "Nessuna descrizione disponibile."}
+                  </p>
+                  <a
+                    href={`https://www.themoviedb.org/tv/${movie.id}`}
+                    target="_blank"
+                    className="btn btn-danger mt-auto"
+                  >
+                    Dettagli
+                  </a>
+                </div>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       ) : (
-        <p>Nessuna serie tv cercata</p>
+        <p className="text-center mt-4">Nothing here</p>
       )}
     </div>
   );

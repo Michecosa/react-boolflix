2aea0de069b7c2b75433d0fed62b2317ef5b4073
Michela <micheladellagatta1@gmail.com>
2025-11-19 17:30:28 +0100

    Create card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,40 @@
+function Card({ movie }) {
+  return (
+    <div className="col-md-3 mb-4" key={movie.id}>
+      <div className="card h-100">
+        {movie.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ) : (
+          <div
+            className="card-img-top d-flex align-items-center justify-content-center bg-secondary text-white"
+            style={{ height: "300px" }}
+          >
+            No image found
+          </div>
+        )}
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{movie.name}</h5>
+          <p className="card-text" style={{ flexGrow: 1 }}>
+            {movie.overview
+              ? movie.overview.length > 100
+                ? movie.overview.slice(0, 100) + "..."
+                : movie.overview
+              : "No description found"}
+          </p>
+          <a
+            href={`https://www.themoviedb.org/tv/${movie.id}`}
+            target="_blank"
+            className="btn btn-danger mt-auto"
+          >
+            Dettagli
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/SerieTv.jsx
+++ src/pages/SerieTv.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import { SearchContext } from "../context/SearchContext";
+import Card from "../components/Card";
 
 function SerieTv() {
   const { movies, setMovies, setQuery } = useContext(SearchContext);
@@ -14,40 +15,7 @@ function SerieTv() {
       {movies.length > 0 ? (
         <div className="row">
           {movies.map((movie) => (
-            <div className="col-md-3 mb-4" key={movie.id}>
-              <div className="card h-100">
-                {movie.poster_path ? (
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt={movie.name}
-                  />
-                ) : (
-                  <div
-                    className="card-img-top d-flex align-items-center justify-content-center bg-secondary text-white"
-                    style={{ height: "300px" }}
-                  >
-                    No image found
-                  </div>
-                )}
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">{movie.name}</h5>
-                  <p className="card-text" style={{ flexGrow: 1 }}>
-                    {movie.overview
-                      ? movie.overview.length > 100
-                        ? movie.overview.slice(0, 100) + "..."
-                        : movie.overview
-                      : "No description found"}
-                  </p>
-                  <a
-                    href={`https://www.themoviedb.org/tv/${movie.id}`}
-                    target="_blank"
-                    className="btn btn-danger mt-auto"
-                  >
-                    Dettagli
-                  </a>
-                </div>
-              </div>
-            </div>
+            <Card key={movie.id} movie={movie} />
           ))}
         </div>
       ) : (

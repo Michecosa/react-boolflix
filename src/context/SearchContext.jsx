a7517fb81f452223e6b90451ac2db94ebfbfbe3f
Michela <micheladellagatta1@gmail.com>
2025-11-19 16:09:35 +0100

    Add SearchContext and SearchProvider for managing movie search state

---
+++ src/context/SearchContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const SearchContext = createContext();
+
+export const SearchProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+
+  return (
+    <SearchContext.Provider value={{ movies, setMovies, query, setQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};

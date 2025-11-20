4984538ca82759ebbbd4397d8db92b4c29accca6
Michela <micheladellagatta1@gmail.com>
2025-11-20 15:38:56 +0100

    Add global state to filter adult content

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -5,9 +5,19 @@ export const SearchContext = createContext();
 export const SearchProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("");
+  const [adultContent, setAdultContent] = useState(false);
 
   return (
-    <SearchContext.Provider value={{ movies, setMovies, query, setQuery }}>
+    <SearchContext.Provider
+      value={{
+        movies,
+        setMovies,
+        query,
+        setQuery,
+        adultContent,
+        setAdultContent,
+      }}
+    >
       {children}
     </SearchContext.Provider>
   );

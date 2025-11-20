10e53c764dd93207d739d8b9f2ff05fb1e3da025
Michela <micheladellagatta1@gmail.com>
2025-11-20 15:16:14 +0100

    Refactor AggiuntiDiRecente and Originali components to load movies from API and handle empty queries

--- src/pages/Originali.jsx
+++ src/pages/Originali.jsx
@@ -1,5 +1,44 @@
+import { useContext, useEffect } from "react";
+import { SearchContext } from "../context/SearchContext";
+import Card from "../components/Card";
+import axios from "axios";
+
 function Originali() {
-  return <div>Originali</div>;
+  const { movies, setMovies, query } = useContext(SearchContext);
+  const api_key = import.meta.env.VITE_TMDB_KEY;
+
+  const loadPopular = () => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}`)
+      .then((response) => {
+        setMovies(response.data.results);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    loadPopular();
+  }, []);
+
+  useEffect(() => {
+    if (!query.trim()) {
+      loadPopular();
+    }
+  }, [query]);
+
+  return (
+    <div className="container mt-4">
+      {movies.length > 0 ? (
+        <div className="row">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center mt-4 text-white">Nothing here</p>
+      )}
+    </div>
+  );
 }
 
 export default Originali;
